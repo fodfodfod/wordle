22d735c699a665eff31cc29a5a74db9553852c4b
fodfodfod <94200657+fodfodfod@users.noreply.github.com>
2022-05-10 21:17:33 -0400

    add fuctionality to wordValidator, wordGenerator, and word lists

--- Wordle/src/WordGenerator.java
+++ Wordle/src/WordGenerator.java
@@ -1,6 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Random;
+import java.util.Scanner;
+
 public class WordGenerator {
-    public static String generateWord(){
-        return "words";
+    private String[] validWords;
+    private Random r;
+    public WordGenerator(){
+        r = new Random();
+        validWords = new String[2315];
+        File file = new File("src/word_lists/wordle_answers.txt");
+        Scanner input = null;
+        try{
+            input = new Scanner(file);
+            for(int i = 0; i<validWords.length;i++){
+                validWords[i] = input.nextLine().trim().toLowerCase();
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("cant open file");
+        }
     }
+    public String generateWord(){
+        return validWords[r.nextInt(2314)];
+    }
+
+    //source for words https://github.com/techtribeyt/Wordle 
     
 }
--- Wordle/src/WordValidator.java
+++ Wordle/src/WordValidator.java
@@ -1,7 +1,63 @@
+import java.nio.file.Path;
+import java.util.Scanner;
+
+import javax.xml.validation.Validator;
+
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+
+
 public class WordValidator {
-    /**NOT DONE 0 is not 5 letters, 1 is 5 letters but not correct, 2 is correct word */
-    public static int validWord(String word){
-        return 2;
+    private String[] validAnswers;
+    
+    public WordValidator(){
+        validAnswers = new String[12942];
+        File file = new File("src/word_lists/wordle_guesses.txt");
+        Scanner input = null;
+        try{
+            input = new Scanner(file);
+            for(int i = 0; i<validAnswers.length;i++){
+                validAnswers[i] = input.nextLine().trim().toLowerCase();
+                
+            }
+        }
+        catch (FileNotFoundException ex){
+            System.out.println("cant open file");
+        }
+        
     }
+    
+    
+    /**NOT DONE 0 is not 5 letters or is not valid answer, 1 is 5 letters and valid answer but not correct, 2 is correct word */
+    public int validWord(String word, String correctWord){
+        //is the word 5 letters
+        if(word.length()!=5){
+            return 0;
+        }
+
+        //is the word in the list
+        boolean isValidWord = false;
+        for(int i = 0; i < validAnswers.length; i++){
+            if(validAnswers[i].equals(word)){
+                isValidWord = true;
+                break;
+            }
+        }
+        if(!isValidWord){
+            return 0;
+        }
+
+        //is the word correct
+        if(!word.equals(correctWord)){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+    //source for words https://github.com/techtribeyt/Wordle 
     
 }
